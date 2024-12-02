5a47acbac9598ab0ad52b7f2dc29067cfb29d8c1
moesandarwin1 <moe550120@gmail.com>
2024-12-02 09:55:20 +0630

    Update

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [App\Http\Controllers\BlogController::class, 'blog'])->name('blog');
+Route::group(['prefix'=>'backend','as'=>'backend.'], function(){
+    Route::get('/', [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
